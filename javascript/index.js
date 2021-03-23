c772611478d048455498fc79ad810bbf11b8a1f5
JessTofunmi <80430649+JessTofunmi@users.noreply.github.com>
2021-03-23 04:52:04 +0100

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+
+
+  var nav4 = document.getElementById('nav1');
+
+  window.onscroll = function() {
+      if (window.scrollY > 0){
+          nav.classlist.add('scroll')
+      } else {
+          nav.classList.remove('scroll')
+      }
+  }
---
+++ javascript/toogle.js
@@ -0,0 +1,6 @@
+var input = document.getElementById('input')
+var button1 = document.getElementById('btn')
+
+button1.addEventListener('click', () =>{
+    input.classList.toggle('active')
+})
