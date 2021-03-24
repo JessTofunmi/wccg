31e4c05b09c81c9c96a40a75e11a474fa4ecd32c
JessTofunmi <80430649+JessTofunmi@users.noreply.github.com>
2021-03-25 00:35:24 +0100

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -1,25 +1,18 @@
+var nav2 = document.getElementById('nav');
 
+window.onscroll = function() {
 
-  var nav4 = document.getElementById('nav1');
-
-  window.onscroll = function() {
-      if (window.scrollY > 0){
-          nav.classlist.add('scroll')
-      } else {
-          nav.classList.remove('scroll')
-      }
+    // pageYOffset or scrollY
+    if (window.scrollY > 0) {
+      nav.classList.add('scroll')
+    } else {
+     nav.classList.remove('scroll')
+    }
   }
 
-  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
   
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
+
+  var btn9 = document.getElementById("btn5")
+  btn5.onclick = function(){
+    location.href = "https://web.facebook.com/bosunoladaniel"
   }
